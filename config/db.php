abaee8dda0446b5e894c365d769f32a00057a819
SkyNet322 <aliev_arkadij@mail.ru>
2019-11-09 13:29:57 +0300

    init commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=books',
+    'username' => 'user',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
